6087d9ad6760ecc46c0e9cbc187806ca579d0eac
onyxbo <buzzardyt712@gmail.com>
2025-10-14 19:59:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,28 @@ eventlet.monkey_patch()  # MUST be first
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 import os
-
+import json
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret123'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+app.config['SECRET_KEY'] = 'secret123'  # change this later
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+CHAT_FILE = 'chat_history.json'
+
+def load_messages():
+    try:
+        with open(CHAT_FILE, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def save_message(msg):
+    messages = load_messages()
+    messages.append(msg)
+    with open(CHAT_FILE, 'w') as f:
+        json.dump(messages, f)
+
+messages = load_messages()
 
 @app.route('/')
 def home():
@@ -16,11 +33,13 @@ def home():
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f'Message: {msg}')
-    send(msg, broadcast=True)  # broadcast to all connected clients
+    messages.append(msg)
+    save_message(msg)
+    send(msg, broadcast=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
+
 
 
