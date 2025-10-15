7829d987c9149f220ea52f168fdd847bb232453d
onyxbo <buzzardyt712@gmail.com>
2025-10-14 19:21:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+rom flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import os
+import eventlet
+
+eventlet.monkey_patch()  # VERY important
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret123'  # change this later
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'secret123'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
 def home():
@@ -12,7 +16,8 @@ def home():
 @socketio.on('message')
 def handle_message(msg):
     print(f'Message: {msg}')
-    send(msg, broadcast=True)  # send to everyone connected
+    send(msg, broadcast=True)  # broadcast to all connected clients
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
